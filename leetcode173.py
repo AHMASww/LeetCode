fe907431cfd1d5c0f3569ec18ac8357166cdcac9
AHMASww <50703927+AHMASww@users.noreply.github.com>
2019-07-22 20:30:53 +0800

    Update leetcode173.py

--- leetcode173.py
+++ leetcode173.py
@@ -4,9 +4,8 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
-# 不符合题目空间要求
-
+'''
+# LDR方法不符合空间要求
 class BSTIterator:
 
     def __init__(self, root: TreeNode):
@@ -35,7 +34,46 @@ class BSTIterator:
             return True
         else:
             return False
-        
+'''
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class BSTIterator:
+
+    def __init__(self, root: TreeNode):
+        self.stack = []
+        while(root):
+            self.stack.append(root)
+            root = root.left
+
+    def next(self) -> int:
+        """
+        @return the next smallest number
+        """
+        cur_node = self.stack.pop()
+        ans = cur_node.val
+        cur_node = cur_node.right
+        while cur_node:
+            self.stack.append(cur_node)
+            cur_node = cur_node.left
+        return ans
+
+    def hasNext(self) -> bool:
+        """
+        @return whether we have a next smallest number
+        """
+        if self.stack: return True
+        else: return False
+
+
+# Your BSTIterator object will be instantiated and called as such:
+# obj = BSTIterator(root)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
 
 
 # Your BSTIterator object will be instantiated and called as such:
